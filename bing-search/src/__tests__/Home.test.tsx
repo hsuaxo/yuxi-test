b5838b9b57bd0a7404ca849519ad54221f666f4f
Hamlet Suazo <hsuazo@hamletsuazo.com>
2023-03-11 20:53:22 -0400

    Add initial jest configuration

---
+++ bing-search/src/__tests__/Home.test.tsx
@@ -0,0 +1,5 @@
+import HomePage from "../pages/Home";
+
+test("Renders home page correctly", () => {
+  expect(true).toBeTruthy();
+});
---
+++ bing-search/src/__tests__/Search.test.tsx
@@ -0,0 +1,5 @@
+import SearchPage from "../pages/Search";
+
+test("Renders search page correctly", () => {
+  expect(true).toBeTruthy();
+});
